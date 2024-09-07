52ea89614348db344cd8ff83368676b8bdcfbfc3
Johannes-UWU <111251913+Johannes-UWU@users.noreply.github.com>
2024-09-07 09:38:34 -0700

    Update Decollector.js

--- js/Decollector.js
+++ js/Decollector.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Send the foods no fucking errors... yeh? no? no? yeh? okay. 
     fetch('https://script.google.com/macros/s/AKfycbxOh9Enn1vqPxeqOZf_yqY0rscTCPJiATUKqnGdD4Kvmv6a-RuEZKQEdwPNFTZc9LRx/exec', {
+      mode: 'no-cors',
       method: 'POST',
-      mode: 'no-cors'
       headers: {
         'Content-Type': 'application/json',
       },
