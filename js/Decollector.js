5329d668978b778b6ebb348df5f9a422306a3aea
Johannes-UWU <111251913+Johannes-UWU@users.noreply.github.com>
2024-09-06 22:48:49 -0700

    Update Decollector.js

--- js/Decollector.js
+++ js/Decollector.js
@@ -48,7 +48,7 @@ function collectPageData() {
 
 function sendDataToGoogleSheet(data) {
    // Send the foods no fucking errors... yeh? no? no? yeh? okay. 
-  fetch('https://script.google.com/macros/s/AKfycbwKlrH3DtiYk8QdIewGxh86rtb6vaVh2hD8h4vQmEjBni80iPvHAFFYU6ek4m53As6vjg/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbzLZUOJ0zFK0P2pjTJEcgTJ961xBeF7vUFriOzf50uFqLz9Y35r-mPp5V6J_AC5ISgj/exec', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
